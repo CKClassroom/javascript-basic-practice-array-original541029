29dae2d52ef0a5cc0ed2ea111ebccaac1ac394b3
ivy <original541029@gmail.com>
2019-01-11 16:01:01 +0800

    add  8 topic

--- array-practice.js
+++ array-practice.js
@@ -1,29 +1,50 @@
 let people = ['Greg', 'Mary', 'Devon', 'James'];
 
-
+// 1. Using a loop, iterate through this array and console.log all of the people.
 for (let val of people) {
   console.log(val);
 }
 
+// 2. Write the command to remove "Greg" from the array.
 const removeGreg = people.splice(0, 1);
 console.log(people)
 
+// 3. Write the command to remove "James" from the array.
 const removeJames = people.splice(2, 1);
 console.log(people)
 
-let addMatt=people.push("Matt");
+// 4. Write the command to add "Matt" to the front of the array.
+let addMatt = people.unshift("Matt");
 console.log(people);
 
-let addIvy=people.push("Ivy");
+// 5. Write the command to add your name to the end of the array.
+let addIvy = people.push("Ivy");
 console.log(people);
-
 
 // 6. Using a loop, iterate through this array and after console.log-ing "Mary", exit from the loop.
+troughLoop();
+function troughLoop(){
+  const repeatAry= people.concat(people);
+  console.log(repeatAry)
+  const len = repeatAry.length;
+  for (let i = 0; i < len ; i++) {
+    if (i>(len/2) && repeatAry[i] == "Devon") {
+      return
+    }else{
+      console.log(repeatAry[i])
+    }
+  }
+}
+
+
 
 // 7. Write the command to make a copy of the array using slice. The copy should NOT include "Mary" or "Matt".
+let newArray = people.slice(2,4);
+console.log(newArray)
 
 // 8. Write the command that gives the indexOf where "Mary" is located.
 
+console.log(people.indexOf("Mary"))
 // 9. Write the command that gives the indexOf where "Foo" is located (this should return -1).
 
 // 10. Redefine the people variable with the value you started with. Using the splice command, remove "Devon" from the array and add "Elizabeth" and "Artie". Your array should look like this when you are done ["Greg", "Mary", "Elizabeth", "Artie", "James"].
